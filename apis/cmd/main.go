6a95c42dd00a51acf565559b9f3eba60522e072e
Ansgar Hoyer <ansgar.hoyer@sap.com>
2025-07-23 11:14:18 +0200

    chore: replace 'konfidence.tools.sap' domain with 'konfidence.cloud'

--- apis/cmd/main.go
+++ apis/cmd/main.go
@@ -125,7 +125,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "b020ec63.konfidence.tools.sap",
+		LeaderElectionID:       "b020ec63.konfidence.cloud",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
