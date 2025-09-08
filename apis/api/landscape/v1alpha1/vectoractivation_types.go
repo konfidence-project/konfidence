d702c59be5f83b5bc0ea0465e697ad82ceddee31
clang-sap <carsten.laengsfeld@sap.com>
2025-09-08 14:17:18 +0200

    feat: add base vectorActivation CRD

---
+++ apis/api/landscape/v1alpha1/vectoractivation_types.go
@@ -0,0 +1,75 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	VectorActivationKind = "VectorActivation"
+
+	// VectorActivationFailed indicates that the vectorActivation failed.
+	VectorActivationFailed = "VectorActivationFailed"
+	// VectorActivationSucceeded indicates that the vectorActivation reconciled successfully.
+	VectorActivationSucceeded = "VectorActivationSucceeded"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// VectorActivationSpec defines the desired state of VectorActivation
+type VectorActivationSpec struct {
+}
+
+// VectorActivationStatus defines the observed state of VectorActivation.
+type VectorActivationStatus struct {
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// VectorActivation is the Schema for the vectoractivations API
+type VectorActivation struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// metadata is a standard object metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty,omitzero"`
+
+	// spec defines the desired state of VectorActivation
+	// +required
+	Spec VectorActivationSpec `json:"spec"`
+
+	// status defines the observed state of VectorActivation
+	// +optional
+	Status VectorActivationStatus `json:"status,omitempty,omitzero"`
+}
+
+// +kubebuilder:object:root=true
+
+// VectorActivationList contains a list of VectorActivation
+type VectorActivationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []VectorActivation `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&VectorActivation{}, &VectorActivationList{})
+}
