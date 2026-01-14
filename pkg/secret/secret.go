9961713bf2e158de733a0bb2a204f6152d74091d
clang-sap <carsten.laengsfeld@sap.com>
2026-01-14 09:25:07 +0100

    fix: refactor get secret by configmap to use reader interface

--- pkg/secret/secret.go
+++ pkg/secret/secret.go
@@ -33,12 +33,12 @@ const (
 )
 
 // GetSecretByConfigMap tries to map a domain name to a secret name using a pre-defined configMap
-func GetSecretByConfigMap(ctx context.Context, k8sClient client.Client, configMapName string,
+func GetSecretByConfigMap(ctx context.Context, reader client.Reader, configMapName string,
 	domainName string) (string, error) {
 	log := logf.FromContext(ctx)
 	configMap := &v1.ConfigMap{}
 	// config map must be in konfidence system namespace
-	err := k8sClient.Get(ctx, types.NamespacedName{
+	err := reader.Get(ctx, types.NamespacedName{
 		Namespace: KonfidenceSystemNamespace,
 		Name:      configMapName,
 	}, configMap)
