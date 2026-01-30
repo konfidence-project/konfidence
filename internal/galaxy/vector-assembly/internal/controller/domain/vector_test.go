b1f6f4cee48f52db6334004b502e6c063956d555
alexandro scalese <alexandro.scalese@sap.com>
2026-01-30 08:42:41 +0100

    test: add hasDrift unit test, add mock

---
+++ internal/galaxy/vector-assembly/internal/controller/domain/vector_test.go
@@ -0,0 +1,132 @@
+package domain
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestDomainVector(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "vector domain Suite")
+}
+
+var _ = Describe("HasDrift", func() {
+
+	It("has drift when lengths differ", func() {
+		desiredArtifacts := make([]Artifact, 3)
+		actualArtifacts := make([]Artifact, 2)
+		Expect(HasDrift(desiredArtifacts, actualArtifacts)).To(BeTrue())
+	})
+
+	It("no drift when lengths are same and artifacts match", func() {
+		desiredArtifacts := []Artifact{
+			{
+				Version: "1.2.3",
+				OcmReference: OcmReference{
+					Component:  "component-a",
+					Repository: "http://localhost:5100",
+				},
+			},
+			{
+				Version: "1.52.0",
+				OcmReference: OcmReference{
+					Component:  "component-b",
+					Repository: "http://localhost:5100",
+				},
+			},
+		}
+
+		actualArtifacts := []Artifact{
+			{
+				Version: "1.2.3",
+				OcmReference: OcmReference{
+					Component:  "component-a",
+					Repository: "http://localhost:5100",
+				},
+			},
+			{
+				Version: "1.52.0",
+				OcmReference: OcmReference{
+					Component:  "component-b",
+					Repository: "http://localhost:5100",
+				},
+			},
+		}
+		Expect(HasDrift(desiredArtifacts, actualArtifacts)).To(BeFalse())
+	})
+
+	It("has drift when lengths are same but artifacts differ", func() {
+		desiredArtifacts := []Artifact{
+			{
+				Version: "1.2.3",
+				OcmReference: OcmReference{
+					Component:  "component-a",
+					Repository: "http://localhost:5100",
+				},
+			},
+			{
+				Version: "1.52.0",
+				OcmReference: OcmReference{
+					Component:  "component-b",
+					Repository: "http://localhost:5100",
+				},
+			},
+		}
+		actualArtifacts := []Artifact{
+			{
+				Version: "1.2.3",
+				OcmReference: OcmReference{
+					Component:  "component-a",
+					Repository: "http://localhost:5100",
+				},
+			},
+			{
+				Version: "1.60.5",
+				OcmReference: OcmReference{
+					Component:  "component-b",
+					Repository: "http://localhost:5100",
+				},
+			},
+		}
+		Expect(HasDrift(desiredArtifacts, actualArtifacts)).To(BeTrue())
+	})
+
+	It("has drift when an artifact is missing in actual", func() {
+		desiredArtifacts := []Artifact{
+			{
+				Version: "1.2.3",
+				OcmReference: OcmReference{
+					Component:  "component-a",
+					Repository: "http://localhost:5100",
+				},
+			},
+			{
+				Version: "1.52.0",
+				OcmReference: OcmReference{
+					Component:  "component-b",
+					Repository: "http://localhost:5100",
+				},
+			},
+		}
+
+		actualArtifacts := []Artifact{
+			{
+				Version: "1.2.3",
+				OcmReference: OcmReference{
+					Component:  "component-a",
+					Repository: "http://localhost:5100",
+				},
+			},
+			{
+				Version: "2.0.0",
+				OcmReference: OcmReference{
+					Component:  "component-c",
+					Repository: "http://localhost:5100",
+				},
+			},
+		}
+		Expect(HasDrift(desiredArtifacts, actualArtifacts)).To(BeTrue())
+	})
+})
