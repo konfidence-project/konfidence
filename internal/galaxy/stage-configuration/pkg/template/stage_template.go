062c3aa24a6924bc9ac301c141ce68f29a8cebdf
clang-sap <carsten.laengsfeld@sap.com>
2026-03-23 14:19:38 +0100

    fix: use custom namespaced name struct to serialize to lower case json fields

--- internal/galaxy/stage-configuration/pkg/template/stage_template.go
+++ internal/galaxy/stage-configuration/pkg/template/stage_template.go
@@ -3,12 +3,16 @@ package template
 import (
 	common "github.com/konfidence-project/crds/api/common/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 // StageTemplate is used to initialize a StageSync object
 type StageTemplate struct {
 	metav1.TypeMeta `json:",inline"`
-	Metadata        types.NamespacedName `json:"metadata,omitempty"`
-	Spec            common.StageSpec     `json:"spec,omitempty"`
+	Metadata        NamespacedName   `json:"metadata"`
+	Spec            common.StageSpec `json:"spec"`
+}
+
+type NamespacedName struct {
+	Namespace string `json:"namespace,omitempty"`
+	Name      string `json:"name"`
 }
