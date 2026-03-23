2d329bf25c3a9d071050c869a17d472d3828c3c3
clang-sap <carsten.laengsfeld@sap.com>
2026-03-23 14:48:41 +0100

    fix: verify vector and artefacts

--- internal/star/vector-deployment/internal/controller/vectordeployment_controller.go
+++ internal/star/vector-deployment/internal/controller/vectordeployment_controller.go
@@ -59,15 +59,6 @@ type VectorDeploymentReconciler struct {
 // +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 // +kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the VectorDeployment object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/reconcile
 func (r *VectorDeploymentReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := logf.FromContext(ctx)
 	log.Info("Reconciling VectorDeployment")
