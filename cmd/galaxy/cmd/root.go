01103f5ff109816a8e6766543239ab2cb7924495
Erik Zimmermann <e.zimmermann@sap.com>
2026-06-05 16:57:00 +0200

    feat: expose metrics

--- cmd/galaxy/cmd/root.go
+++ cmd/galaxy/cmd/root.go
@@ -33,6 +33,7 @@ var (
 var (
 	enableLeaderElection  bool
 	probeAddr             string
+	metricsAddr           string
 	kubernetesServiceHost string
 	kubernetesServicePort int
 	kcpEndpointSlice      string
@@ -82,6 +83,7 @@ func init() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&loggerOpts)))
 
 	rootCmd.PersistentFlags().StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	rootCmd.PersistentFlags().StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metrics endpoint binds to. Use \"0\" to disable.")
 	rootCmd.PersistentFlags().BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
 	rootCmd.PersistentFlags().StringVar(&kcpEndpointSlice, "kcp-endpoint-slice-name", os.Getenv(KcpEndpointSliceEnv),
