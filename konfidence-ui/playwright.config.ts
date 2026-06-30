7b447c25d7384e91d27458155f2373bc73caf63a
Ansgar Hoyer <ansgar.hoyer@sap.com>
2026-06-30 15:48:06 +0200

    feat(ui): add linting + formatting + run formatting

--- konfidence-ui/playwright.config.ts
+++ konfidence-ui/playwright.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-	webServer: {
-		command: 'pnpm build && pnpm preview',
-		port: 4173,
-		reuseExistingServer: !process.env.CI
-	},
-	testMatch: '**/*.e2e.{ts,js}'
+  webServer: {
+    command: "pnpm build && pnpm preview",
+    port: 4173,
+    reuseExistingServer: !process.env.CI,
+  },
+  testMatch: "**/*.e2e.{ts,js}",
 });
