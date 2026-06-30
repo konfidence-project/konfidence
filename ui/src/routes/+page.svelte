3ea4cb2ae88ec5c3d449ab6670226a8ead7b56ba
Ansgar Hoyer <ansgar.hoyer@sap.com>
2026-06-30 11:22:41 +0200

    feat: ui5-webcomponents

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import "@ui5/webcomponents/dist/Button.js";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
+<ui5-button>Hello world!</ui5-button>
