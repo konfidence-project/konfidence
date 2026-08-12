4867fe2d322bbe64be25484c829343ad5c3a6bf5
JNKielmann <jan.kielmann@sap.com>
2026-08-12 13:20:49 +0200

    Fix formatting

--- internal/vectorassembly/internal/vector/vector.go
+++ internal/vectorassembly/internal/vector/vector.go
@@ -57,7 +57,8 @@ func (v Vector) Clone() Vector {
 	return v
 }
 
-func HasDrift(currentVector, desiredVector Vector) bool {	return hasArtifactDrift(currentVector.Artifacts, desiredVector.Artifacts) ||
+func HasDrift(currentVector, desiredVector Vector) bool {	
+	return hasArtifactDrift(currentVector.Artifacts, desiredVector.Artifacts) ||
 		hasVectorConfigDrift(currentVector.VectorConfig, desiredVector.VectorConfig)
 }
 
