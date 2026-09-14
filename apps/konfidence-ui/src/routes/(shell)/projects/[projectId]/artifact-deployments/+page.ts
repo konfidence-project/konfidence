7fcc91c98512c4fe7ab2b274db450fdb504fcb90
Robert Ehni <r.ehni@sap.com>
2026-09-14 10:28:07 +0200

    fix: immediately navigate & show loading indicator

--- apps/konfidence-ui/src/routes/(shell)/projects/[projectId]/artifact-deployments/+page.ts
+++ apps/konfidence-ui/src/routes/(shell)/projects/[projectId]/artifact-deployments/+page.ts
@@ -3,13 +3,13 @@ import { LANDSCAPE_PARAM, VECTOR_DEPLOYMENT_PARAM } from "$lib/artifact-deployme
 import { getApiClient } from "$lib/konfidence-api/client-instance";
 import type { PageLoad } from "./$types";
 
-export const load: PageLoad = async ({ params, url }) => {
+export const load: PageLoad = ({ params, url }) => {
   const { projectId } = params;
   const landscapeId = url.searchParams.get(LANDSCAPE_PARAM) ?? undefined;
   const vectorDeploymentId = url.searchParams.get(VECTOR_DEPLOYMENT_PARAM) ?? undefined;
   const store = new ArtifactDeploymentsStore(getApiClient());
 
-  await store.refresh(projectId, { landscapeId, vectorDeploymentId });
+  void store.refresh(projectId, { landscapeId, vectorDeploymentId });
 
   return {
     landscapeId,
